2c3f1f13cee68d9badfee9a2b9bda9f47920a9e5
danilogazzoli <daniloresende@yahoo.com>
2020-04-24 13:30:33 -0300

    Requirements.txt updated and directory .env was removed

--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/danilo/Área de Trabalho/estudopython/workatolist/work-at-olist/.env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
